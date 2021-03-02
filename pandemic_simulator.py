5e15cc1a0f64b1ee1f5b8e47223c0467ddd092c9
Goose <60141816+elegoose@users.noreply.github.com>
2021-03-02 19:39:19 -0300

    particles can infect each other

--- pandemic_simulator.py
+++ pandemic_simulator.py
@@ -4,7 +4,8 @@
 # Death_rate: probabilidad de muerte por contagio
 # Initial_population: población inicial
 # Days_to_heal: cantidad de días para recuperarse
-
+# Autor : Felipe Torralba
+# Algunos comentarios y variables del código están en ingles por simplicidad y legibilidad de código
 import glfw
 from OpenGL.GL import *
 import sys
@@ -14,6 +15,7 @@ import transformations as tr
 # import json
 import my_shapes as my
 import particleclass as pr
+import numpy as np
 
 
 class Timer:
@@ -23,16 +25,25 @@ class Timer:
         self.seconds_passed = 0
 
     def update(self):
+        self.current_time = glfw.get_time()
         self.seconds_passed = self.current_time - self.initial_time
 
     def seconds_has_passed(self, seconds):
         if self.current_time == 0:
             return False
-        if self.seconds_passed >= seconds:
+        elif self.seconds_passed >= seconds:
             self.initial_time = glfw.get_time()
             return True
         else:
             return False
+
+
+class Virus:
+    def __init__(self, radius, contagious_prob, death_rate, days_to_heal):
+        self.radius = radius
+        self.contagious_prob = contagious_prob
+        self.death_rate = death_rate
+        self.days_to_heal = days_to_heal
 
 
 if __name__ == '__main__':
@@ -59,7 +70,7 @@ if __name__ == '__main__':
     width_square = 1
     height_square = proportion
     radius_circle = 0.02
-    particle_amount = 20
+    particle_amount = 30
     # Creando figuras
     circle = my.createCircle(30, 0, 0, 1, radius_circle, proportion)
     square = my.createSquare(width_square, height_square)
@@ -68,29 +79,62 @@ if __name__ == '__main__':
     gpuCircle = es.toGPUShape(circle)
     gpuSquare = es.toGPUShape(square)
 
+    # Virus variables
+    virus_radius = 0.05
+    virus_contagious_prob = 0.2
+    virus_death_rate = 0.1
+    virus_days_to_heal = 5
+    virus = Virus(virus_radius, virus_contagious_prob, virus_death_rate, virus_days_to_heal)
+
     particle_array = []
     for i in range(particle_amount):
-        particle = pr.Particle(width_square, height_square, radius_circle, proportion)
+        particle = pr.Particle()
+        particle.set_box_limits(width_square, height_square, radius_circle, proportion)
+        particle.set_initial_pos()
         particle.pipeline = figurePipeline
         particle.gpuShape = gpuCircle
-        particle.initial_velocity = 0.01/5
+        particle.initial_velocity = 0.01 / 5
         particle.velocity = particle.initial_velocity
-        # particle.clock.initial_time = glfw.get_time()
+        particle.virus = virus
         particle_array.append(particle)
 
     clock = Timer()
     clock.initial_time = glfw.get_time()
+    dayCounter = Timer()
+    dayCounter.initial_time = glfw.get_time()
+    dayCount = 0
     while not glfw.window_should_close(window):
         glfw.poll_events()
 
         # Clearing the screen in both, color and depth
         glClear(GL_COLOR_BUFFER_BIT)
 
-        clock.current_time = glfw.get_time()
+        dayCounter.update()
+        # Every two seconds, a day passes
+        dayPassed = dayCounter.seconds_has_passed(2)
+        if dayPassed:
+            dayCount += 1
+            print(dayCount)
         clock.update()
-        time_up = clock.seconds_has_passed(3)
+        time_up = clock.seconds_has_passed(5)
         for particle in particle_array:
+
+            aux_particle_array = list(particle_array)
+            aux_particle_array.remove(particle)
+            for aux_particle in aux_particle_array:
+                dx = particle.x - aux_particle.x
+                dy = particle.y - aux_particle.y
+                distance = np.sqrt(dx ** 2 + dy ** 2)
+                if distance < \
+                        particle.radius_circle + aux_particle.radius_circle + virus.radius and \
+                        dayPassed:
+                    choice = np.random.choice([None, 'infect'],
+                                              p=[1 - virus.contagious_prob, virus.contagious_prob])
+                    if choice == 'infect':
+                        aux_particle.infect()
+
             if time_up:
+                # Check random movement events every 5 seconds
                 particle.check_events()
 
             particle.update()
--- particleclass.py
+++ particleclass.py
@@ -1,41 +1,32 @@
 import numpy as np
 from OpenGL.GL import *
-# import glfw
 import transformations as tr
-
-
-# class Timer:
-#     def __init__(self):
-#         self.initial_time = 0
-#         self.current_time = 0
-#         self.seconds_passed = 0
-#
-#     def update(self):
-#         self.seconds_passed = self.current_time - self.initial_time
-#
-#     def seconds_has_passed(self, seconds):
-#         if self.current_time == 0:
-#             return False
-#         if self.seconds_passed >= seconds:
-#             self.initial_time = glfw.get_time()
-#             return True
-#         else:
-#             return False
+import my_shapes as my
+import easy_shaders as es
 
 
 class Particle:
-    def __init__(self, width_square, height_square, radius_circle, proportion):
+    def __init__(self):
 
-        self.x = np.random.uniform(-width_square / 2 + radius_circle, width_square / 2 - radius_circle)
+        self.x, self.y, self.x_vector, self.y_vector = 0, 0, 0, 0
 
-        self.y = np.random.uniform(-height_square / 2 + radius_circle * proportion,
-                                   height_square / 2 - radius_circle * proportion)
+        self.radius_circle = 0
 
-        self.x_vector = np.random.choice([-1, 1])
+        self.width_square, self.height_square, self.proportion = 0, 0, 0
 
-        self.y_vector = np.random.choice([-1, 1])
+        self.pipeline, self.gpuShape = None, None
 
-        self.radius_circle = radius_circle
+        self.activeEvent = None
+
+        self.initial_velocity, self.velocity = 0, 0
+
+        self.acceleration_factor = 1
+
+        self.isInfected = False
+
+        self.virus = None
+
+    def set_box_limits(self, width_square, height_square, radius_circle, proportion):
 
         self.width_square = width_square
 
@@ -43,20 +34,28 @@ class Particle:
 
         self.proportion = proportion
 
-        self.pipeline = None
+        self.radius_circle = radius_circle
 
-        self.gpuShape = None
+    def set_initial_pos(self):
+        self.x = np.random.uniform(-self.width_square / 2 + self.radius_circle,
+                                   self.width_square / 2 - self.radius_circle)
 
-        self.activeEvent = None
+        self.y = np.random.uniform(-self.height_square / 2 + self.radius_circle * self.proportion,
+                                   self.height_square / 2 - self.radius_circle * self.proportion)
 
-        self.initial_velocity = 0
+        self.x_vector = np.random.choice([-1, 1])
 
-        self.velocity = 0
+        self.y_vector = np.random.choice([-1, 1])
 
-        # self.clock = Timer()
+    def set_virus(self, virus):
+        self.virus = virus
 
-        self.acceleration_factor = 1
-
+    def infect(self):
+        self.gpuShape = es.toGPUShape(
+            my.createCircle(30, 1, 0, 0, self.radius_circle, self.proportion)
+        )
+        self.isInfected = True
+        
     def check_events(self):
         if self.activeEvent is not None:
             return
@@ -82,18 +81,30 @@ class Particle:
         self.pipeline.drawShape(self.gpuShape)
 
 
+def select_event(particle):
+    particle.activeEvent = np.random.choice(
+        [None, 'initiate change_direction', 'vertical_acceleration', 'vertical_deceleration'],
+        p=[0.6, 0.1, 0.2, 0.1])
+
+
+def apply_active_event(particle):
+    if particle.activeEvent is None:
+        return
+    elif 'change_direction' in particle.activeEvent:
+        change_direction(particle)
+    elif particle.activeEvent == 'vertical_acceleration':
+        vertical_acceleration(particle)
+    elif particle.activeEvent == 'vertical_deceleration':
+        vertical_deceleration(particle)
+
+
 def change_direction(particle):
-    particle.velocity -= particle.initial_velocity / 30
-    if particle.velocity <= 0:
-        particle.x_vector = - particle.x_vector
-        particle.y_vector = - particle.y_vector
-        particle.activeEvent = 'reverse_change_direction'
-
-
-def reverse_change_direction(particle):
-    particle.velocity += particle.initial_velocity / 30
-    if particle.velocity >= particle.initial_velocity:
-        particle.activeEvent = None
+    if 'decrease_velocity' in particle.activeEvent or \
+            'initiate change_direction' in particle.activeEvent:
+        decrease_velocity(particle)
+    else:
+        # It already is turned around
+        increase_velocity(particle)
 
 
 def vertical_acceleration(particle):
@@ -112,17 +123,19 @@ def vertical_deceleration(particle):
     particle.y_vector = abs(particle.y_vector) * particle.acceleration_factor / particle.y_vector
 
 
-def select_event(particle):
-    particle.activeEvent = np.random.choice(
-        [None, 'change_direction', 'vertical_acceleration', 'vertical_deceleration'], p=[0.6, 0.1, 0.2, 0.1])
+# Para change_direction
+def decrease_velocity(particle):
+    particle.velocity -= particle.initial_velocity / 30
+    particle.activeEvent = '(change_direction) decrease_velocity'
+    if particle.velocity <= 0:
+        # Turn around
+        particle.x_vector = - particle.x_vector
+        particle.y_vector = - particle.y_vector
+        particle.activeEvent = '(change_direction) increase_velocity'
 
 
-def apply_active_event(particle):
-    if particle.activeEvent == 'change_direction':
-        change_direction(particle)
-    elif particle.activeEvent == 'reverse_change_direction':
-        reverse_change_direction(particle)
-    elif particle.activeEvent == 'vertical_acceleration':
-        vertical_acceleration(particle)
-    elif particle.activeEvent == 'vertical_deceleration':
-        vertical_deceleration(particle)
+# Para change_direction
+def increase_velocity(particle):
+    particle.velocity += particle.initial_velocity / 30
+    if particle.velocity >= particle.initial_velocity:
+        particle.activeEvent = None
